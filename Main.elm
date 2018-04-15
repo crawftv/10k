734b331f24d50aed23a4a08d738ccf1ed32c2c5f
crawdogginit <crawford.collins@gmail.com>
2018-04-15 18:27:30 -0500

    got createnewgoalviews working

--- Main.elm
+++ Main.elm
@@ -4,20 +4,23 @@ import Element exposing (..)
 import Style exposing (..)
 import Style.Color as Color
 import Style.Font as Font
+import Element.Input as Input
 import Color exposing (..)
 import Html exposing (Html)
 
 
 type alias Model =
     { currentUser : Maybe User
-    , currentGoal : Maybe Goal
+    , newGoalName : Maybe String
+    , newGoalGoal : Maybe String
+    , newGoalProgress : Maybe String
+    , userGoals : Maybe (List Goal)
     }
 
 
 type alias User =
     { email : String
     , uid : String
-    , userGoals : List Goal
     }
 
 
@@ -30,6 +33,8 @@ type alias Goal =
 
 type Msg
     = LoadUser User
+    | NewGoalName String
+    | NewGoalGoal String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -37,6 +42,12 @@ update msg model =
     case msg of
         LoadUser user ->
             ( { model | currentUser = Just user }, Cmd.none )
+
+        NewGoalName goalName ->
+            ( { model | newGoalName = Just goalName }, Cmd.none )
+
+        NewGoalGoal goalGoal ->
+            ( { model | newGoalGoal = Just goalGoal }, Cmd.none )
 
 
 main =
@@ -50,7 +61,7 @@ main =
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model Nothing Nothing, Cmd.none )
+    ( Model Nothing Nothing Nothing Nothing Nothing, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -64,6 +75,8 @@ port loadUser : (User -> msg) -> Sub msg
 type MyStyles
     = TitleBar
     | CreateGoalView
+    | NoStyle
+    | Error
 
 
 stylesheet =
@@ -78,9 +91,15 @@ stylesheet =
             , Color.background white
             , Font.size 16
             ]
+        , Style.style NoStyle
+            []
+        , Style.style Error
+            [ Color.text Color.red
+            ]
         ]
 
 
+userView : Maybe User -> Element MyStyles variation msg
 userView user =
     case user of
         Nothing ->
@@ -90,17 +109,92 @@ userView user =
             el TitleBar [] (text user.email)
 
 
-createGoalView =
-    row CreateGoalView
+titleView : Model -> Element MyStyles variation msg
+titleView model =
+    userView model.currentUser
+
+
+createGoalView : Model -> Element MyStyles variation Msg
+createGoalView model =
+    Element.column NoStyle
         []
-        [ el CreateGoalView [] (text "Goal Name")
-        , el CreateGoalView [] (text "End Goal")
-        , el CreateGoalView [] (text "Current Progess")
-        ]
+        [ createGoalNameView model.newGoalName, createGoalGoalView model.newGoalGoal, createGoalProgressView model.newGoalProgress ]
+
+
+createGoalNameView : Maybe String -> Element MyStyles variation Msg
+createGoalNameView newGoalName =
+    Input.text CreateGoalView
+        []
+        { onChange = NewGoalName
+        , value =
+            case newGoalName of
+                Nothing ->
+                    ""
+
+                Just newGoalName ->
+                    newGoalName
+        , label =
+            Input.placeholder
+                { label = Input.labelLeft (el NoStyle [] (text "Goal Name: "))
+                , text = "Placeholder!"
+                }
+        , options =
+            [-- [ Input.errorBelow (el Error [] (text "This is an Error!"))
+            ]
+        }
+
+
+createGoalGoalView : Maybe String -> Element MyStyles variation Msg
+createGoalGoalView newGoalGoal =
+    Input.text CreateGoalView
+        []
+        { onChange = NewGoalGoal
+        , value =
+            case newGoalGoal of
+                Nothing ->
+                    ""
+
+                Just newGoalGoal ->
+                    newGoalGoal
+        , label =
+            Input.placeholder
+                { label = Input.labelLeft (el NoStyle [] (text "How many steps is this goal?: "))
+                , text = "10,000"
+                }
+        , options =
+            [-- [ Input.errorBelow (el Error [] (text "This is an Error!"))
+            ]
+        }
+
+
+createGoalProgressView : Maybe String -> Element MyStyles variation Msg
+createGoalProgressView newGoalProgress =
+    Input.text CreateGoalView
+        []
+        { onChange = NewGoalGoal
+        , value =
+            case newGoalProgress of
+                Nothing ->
+                    ""
+
+                Just newGoalGoal ->
+                    newGoalGoal
+        , label =
+            Input.placeholder
+                { label = Input.labelLeft (el NoStyle [] (text "How many steps have you taken on this goal?: "))
+                , text = "0"
+                }
+        , options =
+            [-- [ Input.errorBelow (el Error [] (text "This is an Error!"))
+            ]
+        }
 
 
 view : Model -> Html Msg
 view model =
     Element.layout stylesheet <|
-        Element.h1 TitleBar [] <|
-            userView model.currentUser
+        pageArea model
+
+
+pageArea model =
+    Element.column NoStyle [] [ titleView model, createGoalView model ]
